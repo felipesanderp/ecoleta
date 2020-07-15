5eacb272a5eb8e526be19f486738dbd538b3a944
felipesanderp <felipe.panisson@hotmail.com>
2020-07-14 22:32:48 -0300

    add new route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,23 @@ import express from 'express';
 
 const app = express();
 
+const users = [
+  'Diego',
+  'Felipe',
+  'Cleiton',
+  'Mayk'
+]
+
 app.get('/users', (request, response) => {
-  response.json({ users: 'user-2' })
+  response.json(users)
+})
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
 })
 
 app.listen(3333);
