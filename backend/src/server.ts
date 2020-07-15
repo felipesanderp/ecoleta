ed00b184bc562ceb465c105211a6dd508cbe3031
felipesanderp <felipe.panisson@hotmail.com>
2020-07-15 14:23:43 -0300

    update routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,24 +1,10 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-const users = [
-  'Diego',
-  'Felipe',
-  'Cleiton',
-  'Mayk'
-]
-
-app.get('/users', (request, response) => {
-  response.json(users)
-})
-
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  return response.json(user);
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello World' });
 })
 
 app.listen(3333);
