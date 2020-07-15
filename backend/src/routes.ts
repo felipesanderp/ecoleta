1fbb69a53a0772e160f7b57dfa0881773ce6fb30
felipesanderp <felipe.panisson@hotmail.com>
2020-07-15 17:59:42 -0300

    refactor: routes file, add controllers

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,58 +1,14 @@
 import express from 'express';
-import knex from './database/connection';
+
+import ItemsController from './controllers/ItemsContoller';
+import PointsController from './controllers/PointsController';
 
 const routes = express.Router();
+const itemsController = new ItemsController();
+const pointsController = new PointsController();
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('items').select('*');
-  
-  const serializedItems = items.map(item => {
-    return { 
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`
-     };
-  })
+routes.get('/items', itemsController.index);
 
-  response.json(serializedItems);
-})
-
-routes.post('/points', async (request, response) => {
-  const { 
-    name, 
-    email, 
-    whatsapp, 
-    latitude, 
-    longitude, 
-    city, 
-    uf, 
-    items } = request.body;
-
-    const trx = await knex.transaction();
-
-    const insertedIds = await trx('points').insert({
-      image: 'image-fake',
-      name, 
-      email, 
-      whatsapp, 
-      latitude, 
-      longitude, 
-      city, 
-      uf, 
-    });
-
-    const point_id = insertedIds[0];
-
-    const pointItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id,
-      }
-    })
-
-    await trx('point_items').insert(pointItems)
-
-    return response.json({ sucess: true });
-});
+routes.post('/points', pointsController.create);
 
 export default routes;
